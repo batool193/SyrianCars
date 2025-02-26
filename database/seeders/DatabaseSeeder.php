4c783650bf32d10e1123e05d673752bc1efc2e51
TarekAlhajjeh <150261555+TarekAlhajjeh@users.noreply.github.com>
2025-02-26 23:45:57 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,25 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            RolesSeeder::class,
+            UsersSeeder::class,
+            ColorsSeeder::class,
+            BrandsSeeder::class,
+            ModelsSeeder::class,
+            CitiesSeeder::class,
+            ProductionYearsSeeder::class,
+            FuelsSeeder::class,
+            CarsSeeder::class,
+            RatingsSeeder::class,
+            SocialAccountsSeeder::class,
+            PhotosSeeder::class,
+        ]);
     }
 }
