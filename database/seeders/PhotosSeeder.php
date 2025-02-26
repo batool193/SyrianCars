8859716bcaa83641094798957232015a428f6771
TarekAlhajjeh <150261555+TarekAlhajjeh@users.noreply.github.com>
2025-02-26 23:43:32 +0300

    Create PhotosSeeder.php

---
+++ database/seeders/PhotosSeeder.php
@@ -0,0 +1,21 @@
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PhotosSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('photos')->insert([
+            [
+                'id' => 1,
+                'photoable_type' => 'car',
+                'photoable_id' => 1,
+                'url' => 'https://example.com/car1.jpg',
+                'name' => 'car_photo.jpg',
+                'created_at' => now(),
+            ],
+        ]);
+    }
+}
