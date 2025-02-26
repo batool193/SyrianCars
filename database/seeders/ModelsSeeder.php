357dfbea6540d666d730494a212fb28cfde24221
TarekAlhajjeh <150261555+TarekAlhajjeh@users.noreply.github.com>
2025-02-26 23:34:50 +0300

    Create ModelsSeeder.php

---
+++ database/seeders/ModelsSeeder.php
@@ -0,0 +1,17 @@
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ModelsSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('models')->insert([
+            ['id' => 1, 'brand_id' => 1, 'name' => 'Corolla'],
+            ['id' => 2, 'brand_id' => 1, 'name' => 'Camry'],
+            ['id' => 3, 'brand_id' => 2, 'name' => 'Mustang'],
+            ['id' => 4, 'brand_id' => 3, 'name' => 'X5'],
+        ]);
+    }
+}
