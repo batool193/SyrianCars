c4eb283b882888f9323b5ebb9f224f8be8a41c00
TarekAlhajjeh <150261555+TarekAlhajjeh@users.noreply.github.com>
2025-02-26 23:32:47 +0300

    Create ColorsSeeder.php

---
+++ database/seeders/ColorsSeeder.php
@@ -0,0 +1,20 @@
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ColorsSeeder extends Seeder
+{
+    public function run()
+    {
+        $colors = ['Red', 'Blue', 'Black', 'White', 'Green', 'Yellow', 'Gray'];
+
+        foreach ($colors as $index => $color) {
+            DB::table('colors')->insert([
+                'id' => $index + 1,
+                'name' => $color,
+                'created_at' => now()
+            ]);
+        }
+    }
+}
