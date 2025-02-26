9ef4b049faf0086a2e022ba1da44f5f94c2a82d0
TarekAlhajjeh <150261555+TarekAlhajjeh@users.noreply.github.com>
2025-02-27 00:08:42 +0300

    Update BrandsSeeder.php

--- database/seeders/BrandsSeeder.php
+++ database/seeders/BrandsSeeder.php
@@ -9,9 +9,9 @@ class BrandsSeeder extends Seeder
     public function run()
     {
         $brands = [
-            ['id' => 1, 'name' => 'Toyota', 'founded_year' => 1937, 'country' => 'Japan'],
-            ['id' => 2, 'name' => 'Ford', 'founded_year' => 1903, 'country' => 'USA'],
-            ['id' => 3, 'name' => 'BMW', 'founded_year' => 1916, 'country' => 'Germany'],
+            ['id' => 1, 'name' => 'Toyota', 'founded_year' => 2024, 'country' => 'Japan'],
+            ['id' => 2, 'name' => 'Ford', 'founded_year' => 2015, 'country' => 'USA'],
+            ['id' => 3, 'name' => 'BMW', 'founded_year' => 2025, 'country' => 'Germany'],
         ];
 
         DB::table('brands')->insert($brands);
