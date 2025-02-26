6aab7ead6bb1cb5cb8635967a5ce4fafc191ac26
TarekAlhajjeh <150261555+TarekAlhajjeh@users.noreply.github.com>
2025-02-26 23:33:49 +0300

    Create BrandsSeeder.php

---
+++ database/seeders/BrandsSeeder.php
@@ -0,0 +1,18 @@
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BrandsSeeder extends Seeder
+{
+    public function run()
+    {
+        $brands = [
+            ['id' => 1, 'name' => 'Toyota', 'founded_year' => 1937, 'country' => 'Japan'],
+            ['id' => 2, 'name' => 'Ford', 'founded_year' => 1903, 'country' => 'USA'],
+            ['id' => 3, 'name' => 'BMW', 'founded_year' => 1916, 'country' => 'Germany'],
+        ];
+
+        DB::table('brands')->insert($brands);
+    }
+}
